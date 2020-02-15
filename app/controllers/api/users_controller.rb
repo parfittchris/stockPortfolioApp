a48e90e4433991b70c0aa9d4dbcc0c9aa2a75c6d
Chris Parfitt <parfitt.chris7@gmail.com>
2020-02-15 09:33:13 -0500

    push backend to github

---
+++ app/controllers/api/users_controller.rb
@@ -0,0 +1,38 @@
+class Api::UsersController < ApplicationController
+
+    def create
+        @user = User.new(user_params)
+        if @user.save
+            login!(@user)
+            render "api/users/show"
+        else
+            render json: @user.errors.full_messages, status: 422
+        end
+    end
+
+    def show
+        @user = User.find_by_id(params[:id])
+        
+        if @user.nil?
+            render json: "Can't find user"
+        else
+           render "api/users/show" 
+        end
+    end
+
+    def destroy
+        @user = User.find_by_id(params[:id])
+
+        if @user.nil?
+            render json: "Can't find user"
+        else
+            @user.destroy
+        end
+    end
+
+    private
+
+    def user_params
+        params.require(:user).permit(:username, :email, :password)
+    end
+end
---
+++ app/controllers/application_controller.rb
@@ -0,0 +1,24 @@
+class ApplicationController < ActionController::Base
+    helper_method :current_user, :logged_in?
+
+    def login!(user)
+        session[:session_token] = user.session_token
+    end
+
+    def current_user
+        return nil unless session[:session_token]
+        @current_user ||= User.find_by_session_token(session[:session_token])
+    end
+
+    def logged_in?
+        !current_user.nil?
+    end
+
+    def require_logged_out
+        redirect_to user_url(current_user) if logged_in?
+    end
+
+    def require_logged_in
+        redirect_to api_session unless logged_in?
+    end
+end
